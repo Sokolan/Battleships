b849ab317a6969ca273ca1091639ce6a1b2ef270
Sokolan <valerysokolan@gmail.com>
2023-07-25 18:24:03 +0300

    Revert "project set up"
    This reverts commit 6bf5d8e76450227e8a1f07249b89099a35ce4142.

--- dist/main.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-console.log("Hello world");
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLDJCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vYmF0dGxlc2hpcHMvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJIZWxsbyB3b3JsZFwiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
