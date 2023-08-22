6ac18fcfd77e9e627b9bac1efd8cf58a37510e49
Sokolan <valerysokolan@gmail.com>
2023-08-22 15:36:16 +0300

    initiated GameBoard factory function

--- src/js/modules/Gameboard.js
+++ src/js/modules/Gameboard.js
@@ -1,3 +1,21 @@
-const Gameboard = () => {}
+const Gameboard = () => {
+  const shipsBoard = Array(10).fill(Array(10).fill(null));
+  const hitsBoard = Array(10).fill(Array(10).fill(""));
+  
+  const getHitsBoard = () => {};
+
+  const placeShip = (startCoordination, length, orientation) => {};
+
+  const recieveAttack = (coordination) => {};
+
+  const allShipsSunk = () => {};
+
+  return {
+    getHitsBoard,
+    placeShip,
+    recieveAttack,
+    allShipsSunk,
+  }
+}
 
 export default Gameboard;
