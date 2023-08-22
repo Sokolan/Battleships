6b0e96d37b80540fc3116c68c56649c6b1a0abcf
Sokolan <valerysokolan@gmail.com>
2023-08-22 18:27:44 +0300

    added descriptions to functions

--- src/js/modules/Gameboard.js
+++ src/js/modules/Gameboard.js
@@ -1,13 +1,26 @@
 const Gameboard = () => {
   const mShipsBoard = Array(10).fill(Array(10).fill(null));
   const mHitsBoard = Array(10).fill(Array(10).fill(""));
-  
+
+  /* returns a board with:
+   * "o" - for tile that had ship which was hit
+   * "x" - for tile that was hit and it has no ship
+   * ""  - for tile that wasn't hit
+   */
   const getHitsBoard = () => {};
 
+  // returns boolean value depends on the success of the placing the ship
   const placeShip = (startCoordination, length, orientation) => {};
 
+  /*
+   * returns:
+   *   "hit"  - if there was a hit
+   *   "miss" - if there wasn't a hit
+   *   "EROR" - if the coordination is ilegal (out of scope, coordination already been hit)
+   */
   const recieveAttack = (coordination) => {};
 
+  // returns boolean with answer
   const allShipsSunk = () => {};
 
   return {
@@ -15,7 +28,7 @@ const Gameboard = () => {
     placeShip,
     recieveAttack,
     allShipsSunk,
-  }
-}
+  };
+};
 
 export default Gameboard;
