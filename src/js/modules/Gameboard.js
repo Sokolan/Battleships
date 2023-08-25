fa171ab40aec0e92cbf85528380e2a084c013501
Sokolan <valerysokolan@gmail.com>
2023-08-26 01:13:29 +0300

    recieveAttack done and tested

--- src/js/modules/Gameboard.js
+++ src/js/modules/Gameboard.js
@@ -21,13 +21,6 @@ const Gameboard = () => {
     return true;
   };
 
-  /* returns a board with:
-   * "o" - for tile that had ship which was hit
-   * "x" - for tile that was hit and it has no ship
-   * ""  - for tile that wasn't hit
-   */
-  const getHitsBoard = () => [...mHitsBoard];
-
   const mCheckForNoShips = (coordination, length, orientation) => {
     const fromX = coordination[0] - 1;
     const fromY = coordination[1] - 1;
@@ -49,6 +42,69 @@ const Gameboard = () => {
     }
     return true;
   };
+
+  const mMarkSurroundingOfSunkShip = (coordinate) => {
+    // We will find the start of the ship and determinate if it's horizontal or vertical
+    let x = coordinate[0];
+    let y = coordinate[1];
+    let orientation;
+    // Check if horizontal
+    if (
+      (mCoordinateValid([x, y + 1]) && mHitsBoard[x][y + 1] === "o") ||
+      (mCoordinateValid([x, y - 1]) && mHitsBoard[x][y - 1] === "o")
+    ) {
+      orientation = "horizontal";
+    }
+    else {
+      orientation = "vertical";
+    }
+    
+    while (mCoordinateValid([x, y]) && mHitsBoard[x][y] === "o") {
+      if (orientation === "horizontal") {
+        y -= 1;
+      }
+      else {
+        x -= 1;
+      }
+    }
+    if (orientation === "horizontal"){
+      while (
+        (mCoordinateValid([x, y]) && mHitsBoard[x][y] === "o") ||
+        (mCoordinateValid([x, y - 1]) && mHitsBoard[x][y - 1] === "o") ||
+        (mCoordinateValid([x, y + 1]) && mHitsBoard[x][y + 1] === "o")
+      ) {
+        for (let i = -1; i <= 1; i += 1) {
+          // Mark above and below, and if it's not ship tile then the outer wall too
+          if(mCoordinateValid([x + i, y]) && mHitsBoard[x + i][y] !== "o") {
+            mHitsBoard[x + i][y] = "x";
+          }
+        }
+        y += 1;
+      }
+    }
+    else {
+      while (
+        (mCoordinateValid([x, y]) && mHitsBoard[x][y] === "o") ||
+        (mCoordinateValid([x - 1, y]) && mHitsBoard[x - 1][y] === "o") ||
+        (mCoordinateValid([x + 1, y]) && mHitsBoard[x + 1][y] === "o")
+      ) {
+        for (let i = -1; i <= 1; i += 1) {
+          // Mark above and below, and if it's not ship tile then the outer wall too
+          if(mCoordinateValid([x, y + i]) && mHitsBoard[x][y + i] !== "o") {
+            mHitsBoard[x][y + i] = "x";
+          }
+        }
+        x += 1;
+      }
+    }    
+  };
+
+  /* returns a board with:
+   * "o" - for tile that had ship which was hit
+   * "x" - for tile that was hit and it has no ship
+   * ""  - for tile that wasn't hit
+   */
+  const getHitsBoard = () => [...mHitsBoard];
 
   // returns boolean value depends on the success of the placing the ship
   const placeShip = (startCoordination, length, orientation) => {
@@ -72,7 +128,7 @@ const Gameboard = () => {
     const x = startCoordination[0];
     const y = startCoordination[1];
 
-    const ship = Ship();
+    const ship = Ship(length);
     for (let i = 0; i < length; i += 1) {
       if (orientation === "horizontal") {
         mShipsBoard[x][y + i] = ship;
@@ -87,9 +143,35 @@ const Gameboard = () => {
    * returns:
    *   "hit"  - if there was a hit
    *   "miss" - if there wasn't a hit
-   *   "EROR" - if the coordination is ilegal (out of scope, coordination already been hit)
+   *   "ERROR" - if the coordination is ilegal (out of scope, coordination already been hit)
    */
-  const recieveAttack = (coordination) => {};
+  const recieveAttack = (coordination) => {
+    
+    if (!mCoordinateValid(coordination)) {
+      return "ERROR";
+    }
+    const x = coordination[0];
+    const y = coordination[1];
+
+    if (mHitsBoard[x][y] === "o" || mHitsBoard[x][y] === "x") {
+      return "ERROR";
+    }
+
+    if (mShipsBoard[x][y] === null) {
+      mHitsBoard[x][y] = "x";
+      return "miss";
+    }
+    
+    // else we have a ship at [x][y] and that spot wasn't hit yet
+    mHitsBoard[x][y] = "o";
+    mShipsBoard[x][y].hit();
+
+    // if ship was sank need to mark all surrounding as hit
+    if (mShipsBoard[x][y].isSunk()) {
+      mMarkSurroundingOfSunkShip(coordination);
+    }
+    return "hit";
+  };
 
   // returns boolean with answer
   const allShipsSunk = () => {};
