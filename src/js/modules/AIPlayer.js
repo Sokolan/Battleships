02d6ddbe9a99b3fae6febccec8a3dc412f60b27c
Sokolan <valerysokolan@gmail.com>
2023-08-28 22:44:40 +0300

    added method requiremenets

--- src/js/modules/AIPlayer.js
+++ src/js/modules/AIPlayer.js
@@ -3,8 +3,23 @@ import Player from "./Player";
 const AIPlayer = () => {
   const player = Player("AI");
 
-  player.makeMove = (enemyBoard) => {};
+  // Enemy board should have receiveAttack and getHitsBoard
+  player.makeMove = (enemyBoard) => {
+    const board = enemyBoard.getHitsBoard();
+    let legalMoveFound = false;
+    let result;
+    while (!legalMoveFound) {
+      const x = Math.floor(Math.random() * 10);
+      const y = Math.floor(Math.random() * 10); 
+      if (board[x][y] === "") {
+        legalMoveFound = true;
+        result = enemyBoard.recieveAttack([x, y]);
+      }
+    };
+    return result;
+  };
 
+  // Enemy board should have placeShip
   player.placeShips = (gameboard) => {};
 
   return player;
--- src/js/modules/HumanPlayer.js
+++ src/js/modules/HumanPlayer.js
@@ -3,8 +3,10 @@ import Player from "./Player";
 const HumanPlayer = () => {
   const player = Player("Human");
 
+  // Enemy board should have receiveAttack and getHitsBoard
   player.makeMove = (enemyBoard, coordination) => {};
 
+  // Enemy board should have placeShip
   player.placeShips = (
     fourTileLocations,
     fourTileOrientations,
