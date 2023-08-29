a93a00e073b2d8e23109f068f3b0e216c16a9097
Sokolan <valerysokolan@gmail.com>
2023-08-29 18:38:38 +0300

    placeShip method written with default positioning and tested

--- src/js/modules/AIPlayer.js
+++ src/js/modules/AIPlayer.js
@@ -11,30 +11,44 @@ const AIPlayer = () => {
     const boardSize = enemyBoard.getBoardSize();
     while (!legalMoveFound) {
       const x = Math.floor(Math.random() * boardSize);
-      const y = Math.floor(Math.random() * boardSize); 
+      const y = Math.floor(Math.random() * boardSize);
       if (board[x][y] === "") {
         legalMoveFound = true;
         result = enemyBoard.recieveAttack([x, y]);
       }
-    };
+    }
     return result;
   };
 
   // Enemy board should have placeShip
   /* default board:
-  *  [3, ,2,2, ,1, ,2, , ]
-  *  [3, , , , , , ,2, , ]
-  *  [3, ,3,3,3, , , , , ]
-  *  [ , , , , , ,4,4,4,4]
-  *  [1, , , , , , , , , ]
-  *  [ , ,2,2, , , , , , ]
-  *  [1, , , , , , , , , ]
-  *  [ , , , , , , , , , ]
-  *  [ , , , , , , , ,1, ]
-  *  [ , , , , , , , , , ]
-  */
+   *     0 1 2 3 4 5 6 7 8 9
+   *  0 [3, ,2,2, ,1, ,2, , ]
+   *  1 [3, , , , , , ,2, , ]
+   *  2 [3, ,3,3,3, , , , , ]
+   *  3 [ , , , , , ,4,4,4,4]
+   *  4 [1, , , , , , , , , ]
+   *  5 [ , ,2,2, , , , , , ]
+   *  6 [1, , , , , , , , , ]
+   *  7 [ , , , , , , , , , ]
+   *  8 [ , , , , , , , ,1, ]
+   *  9 [ , , , , , , , , , ]
+   */
 
-  player.placeShips = (gameboard, defaultArrangement = true) => {};
+  player.placeShips = (gameboard, defaultArrangement = true) => {
+    if (defaultArrangement) {
+      gameboard.placeShip([0, 0], 3, "vertical");
+      gameboard.placeShip([0, 2], 2, "horizontal");
+      gameboard.placeShip([0, 5], 1);
+      gameboard.placeShip([0, 7], 2, "vertical");
+      gameboard.placeShip([2, 2], 3, "horizontal");
+      gameboard.placeShip([3, 6], 4, "horizontal");
+      gameboard.placeShip([4, 0], 1);
+      gameboard.placeShip([5, 2], 2, "horizontal");
+      gameboard.placeShip([6, 0], 1);
+      gameboard.placeShip([8, 8], 1);
+    }
+  };
 
   return player;
 };
