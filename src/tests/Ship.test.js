3148a6441d6b99e272e1fc2dadfd1efa332c83d4
Sokolan <valerysokolan@gmail.com>
2023-08-29 21:13:48 +0300

    refactoring tests messages

--- src/tests/Ship.test.js
+++ src/tests/Ship.test.js
@@ -1,38 +1,39 @@
 import Ship from "../js/modules/Ship";
 
-describe("tests to make sure created and they're not sunk", () => {
-  test("test: 1 tile long ship", () => {
+describe("Ship()", () => {
+  test("TEST: newly created 1 tile long ship isn't sunk", () => {
     const ship = Ship(1);
     expect(ship.isSunk()).toBe(false);
   })
   
-  test("test: 4 tiles long ship", () => {
+  test("TEST: newly created 4 tile long ship isn't sunk", () => {
     const ship = Ship(4);
     expect(ship.isSunk()).toBe(false);
   })
 })
 
-describe("test sinking ships", () => {
-  test("test: sink 1 tile long ship", () => {
+describe("isSunk()", () => {
+  test("TEST: 1 tile long ship return true after 1 hit", () => {
     const ship = Ship(1);
+    expect(ship.isSunk()).toBe(false);
     ship.hit();
     expect(ship.isSunk()).toBe(true);
   })
-  describe("test that it takes 4 hits to sink a 4 tiles ship", () => {
+  describe("4 tile long ship should return true only after 4 hits", () => {
     const ship = Ship(4);
-    test("test: 1st hit", () => {
+    test("1st hit should return false", () => {
       ship.hit();
       expect(ship.isSunk()).toBe(false);
     })
-    test("test: 2nd hit", () => {
+    test("2nd hit should return false", () => {
       ship.hit();
       expect(ship.isSunk()).toBe(false);
     })
-    test("test: 3rd hit", () => {
+    test("3rd hit should return false", () => {
       ship.hit();
       expect(ship.isSunk()).toBe(false);
     })
-    test("test: 4th hit, sunk", () => {
+    test("4th hit should return true", () => {
       ship.hit();
       expect(ship.isSunk()).toBe(true);
     })
