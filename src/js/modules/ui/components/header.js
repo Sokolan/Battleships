df5f6b0c05878620529fb53a4901e165c387c3fd
Sokolan <valerysokolan@gmail.com>
2023-09-04 19:51:23 +0300

    basic header

--- src/js/modules/ui/components/header.js
+++ src/js/modules/ui/components/header.js
@@ -1,7 +1,18 @@
+import '../../../../styles/header.css';
 import createElement from "../util/utils";
+
+const createTitle = () => {
+  const container = createElement('div', ['header-title']);
+  const paragraph = createElement('p');
+  paragraph.textContent = "BattleShips";
+
+  container.appendChild(paragraph);
+  return container;
+}
 
 const Header = () => {
   const header = createElement('header');
+  header.appendChild(createTitle());
 
   return header;
 }
