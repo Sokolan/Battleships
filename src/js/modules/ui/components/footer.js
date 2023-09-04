dc9168cca14bd5664fb1ece3b595cdfeb141299d
Sokolan <valerysokolan@gmail.com>
2023-09-04 19:41:56 +0300

    basic footer with git link and name created

--- src/js/modules/ui/components/footer.js
+++ src/js/modules/ui/components/footer.js
@@ -1,9 +1,35 @@
+import "../../../../styles/footer.css";
+import gitImg from "../../../../assets/imgs/github.png";
 import createElement from "../util/utils";
 
-const Footer = () => {
-  const footer = createElement('footer');
+// Create git link with git icon
+const createGitLinkImg = () => {
+  const gitLink = createElement(
+    "a",
+    [],
+    [
+      ["href", "https://github.com/Sokolan/Battleships"],
+      ["target", "_blank"],
+    ],
+  );
+  const gitIcon = createElement('img', ["git-img"], [["src", gitImg]]);
+  gitLink.appendChild(gitIcon);
+  return gitLink;
+};
 
-  return footer;
-}
+const createCreatorName = (creatorName) => {
+  const creator = createElement("p", ["creator"]);
+  creator.textContent = creatorName;
+  return creator;
+};
+
+const Footer = () => {
+
+  const footerContainer = createElement("footer");
+
+  footerContainer.appendChild(createGitLinkImg());
+  footerContainer.appendChild(createCreatorName("Sokolan"));
+  return footerContainer;
+};
 
 export default Footer;
